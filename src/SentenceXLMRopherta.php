93bcbaf177d8ec119bffee1cb9a78fe0ad43a2b2
Pablo Duboue <pablo.duboue@gmail.com>
2024-02-12 19:15:49 -0800

    New ropherta version + Bug fixing

--- src/SentenceXLMRopherta.php
+++ src/SentenceXLMRopherta.php
@@ -17,12 +17,12 @@ class SentenceXLMRopherta extends \Textualization\Ropherta\RophertaModel {
 
     public function _encode(array|string $text, float|bool $padding = false) : array
     {
-        return _encode_passage($text, $padding);
+        return $this->_encode_passage($text, $padding);
     }
     
     public function _encode_passage(array|string $text, float|bool $padding = false) : array
     {
-        return _encode_call("passage: $text", $padding);
+        return $this->_encode_call("passage: $text", $padding);
     }
     
     public function _encode_call(array|string $text, float|bool $padding = false) : array
@@ -55,7 +55,29 @@ class SentenceXLMRopherta extends \Textualization\Ropherta\RophertaModel {
         $zeros = array_fill(0, count($tokens), 0);
         $input[ 'token_type_ids' ] = [ $zeros ];
         
-        return $this->model->predict($input);
+        $full_output = $this->model->predict($input);
+        $layer = $full_output['last_hidden_state'];
+
+        // mean pool
+        $pool = $layer[0][0];
+        $len = count($pool);
+        $wlen = count($layer[0]);
+        for($i=1; $i<$wlen; $i++)
+            for($j=0; $j<$len; $j++)
+                $pool[$j] += $layer[0][$i][$j];
+        $sum_of_sqrs = 0.0;
+        for($j=0; $j<$len; $j++) {
+            $pool[$j] /= $wlen;
+            $sum_of_sqrs += $pool[$j]*$pool[$j];
+        }
+
+        // normalize
+        $norm = sqrt($sum_of_sqrs);
+        for($j=0; $j<$len; $j++) {
+            $pool[$j] /= $norm;
+        }
+
+        return $pool;
     }
 
     public function _encode_query(array|string $text, float|bool $padding = false) : array
