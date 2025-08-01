106b4c59b987e6f9cfe24fd5b9c28be14eddeaca
Pablo Duboue <pablo.duboue@gmail.com>
2025-08-01 10:58:54 -0700

    Changes to support HF Xet migration

--- src/Vendor.php
+++ src/Vendor.php
@@ -35,9 +35,10 @@ class Vendor {
         
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_VERBOSE, false);
+        curl_setopt($ch, CURLOPT_VERBOSE, false;
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, true);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         $contents = curl_exec($ch);
         curl_close($ch);
